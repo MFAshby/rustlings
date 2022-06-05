050e3f6e35d31f512a5e2b6436e23fa192e9d899
Martin Ashby <martin@ashbysoft.com>
2022-06-05 21:35:34 +0100

    First lot

--- exercises/collections/hashmap2.rs
+++ exercises/collections/hashmap2.rs
@@ -35,9 +35,10 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     ];
 
     for fruit in fruit_kinds {
-        // TODO: Put new fruits if not already present. Note that you
-        // are not allowed to put any type of fruit that's already
-        // present!
+        // if let None = basket.get(&fruit) {
+        //     basket.insert(fruit, 1);
+        // }
+        _ = basket.try_insert(&fruit, 1);
     }
 }
 
