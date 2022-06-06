0b8b6620d04f777cd341a0c38d820bf5736831e8
Martin Ashby <martin@ashbysoft.com>
2022-06-06 23:44:37 +0100

    Fourth lot

--- exercises/standard_library_types/iterators3.rs
+++ exercises/standard_library_types/iterators3.rs
@@ -6,8 +6,6 @@
 //    list_of_results functions.
 // Execute `rustlings hint iterators3` to get some hints!
 
-// I AM NOT DONE
-
 #[derive(Debug, PartialEq, Eq)]
 pub enum DivisionError {
     NotDivisible(NotDivisibleError),
@@ -22,20 +20,71 @@ pub struct NotDivisibleError {
 
 // Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
-pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {}
+pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
+    if b == 0 {
+        Err(DivisionError::DivideByZero)
+    } else if a % b != 0 {
+        Err(DivisionError::NotDivisible(NotDivisibleError {dividend: a, divisor: b}))
+    } else {
+        Ok(a / b)
+    }
+}
 
 // Complete the function and return a value of the correct type so the test passes.
 // Desired output: Ok([1, 11, 1426, 3])
-fn result_with_list() -> () {
+fn result_with_list() -> Result<Vec<i32>, DivisionError> {
     let numbers = vec![27, 297, 38502, 81];
     let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    // Longer code, might be more efficient due to single pass over the list?
+    // let mut rr = Vec::new();
+    // for r in division_results {
+    //     match r {
+    //         Err(de) => {
+    //             return Err(de);
+    //         },
+    //         Ok(ri) => {
+    //             rr.push(ri);
+    //         },
+    //     }
+    // }
+    // Ok(rr)
+
+    // Shorter code, more logical too, but possibly slower in the case when everything is ok
+    // because of multiple passes over the iterator (and therefore it'll apply the map and therefore the multiplication?)
+    // if let Some(err) = division_results.clone().find(|r| r.is_err()) {
+    //     Err(err.unwrap_err())
+    // } else {
+    //     Ok(division_results.map(|r| r.unwrap()).collect())
+    // }
+
+    // Fold might do it too. A bit longer than above, slightly more efficient but 
+    // possibly not as efficient as the first one in the error case 
+    // (it will complete a whole pass through division_results I think, wheras the first one will short circuit)
+    let zero = Ok::<Vec<i32>,DivisionError>(Vec::new());
+    division_results.fold(zero, |res, e| {
+        if res.is_err() {
+            res //  If we've already encountered an error, short circuit
+        }
+        else {
+            match e {
+                Err(e) => {
+                    Err(e)
+                },
+                Ok(i) => {
+                    let mut v = res.unwrap();
+                    v.push(i);
+                    Ok(v)
+                }
+            }
+        }
+    })
 }
 
 // Complete the function and return a value of the correct type so the test passes.
 // Desired output: [Ok(1), Ok(11), Ok(1426), Ok(3)]
-fn list_of_results() -> () {
+fn list_of_results() -> Vec<Result<i32, DivisionError>> {
     let numbers = vec![27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    numbers.into_iter().map(|n| divide(n, 27)).collect()
 }
 
 #[cfg(test)]
--- exercises/standard_library_types/iterators5.rs
+++ exercises/standard_library_types/iterators5.rs
@@ -13,6 +13,7 @@
 // I AM NOT DONE
 
 use std::collections::HashMap;
+use std::iter::Iterator;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 enum Progress {
@@ -32,8 +33,7 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 }
 
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
-    // map is a hashmap with String keys and Progress values.
-    // map = { "variables1": Complete, "from_str": None, ... }
+    ct_iter(map.values().into_iter(), value)
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -49,9 +49,13 @@ fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progres
 }
 
 fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
-    // collection is a slice of hashmaps.
-    // collection = [{ "variables1": Complete, "from_str": None, ... },
-    //     { "variables2": Complete, ... }, ... ]
+    ct_iter(collection.into_iter().map(|hm| hm.values()).flatten(), value)
+}
+
+// Refactored both teh count_iterator ad count_collection_iterator to call this one
+fn ct_iter<'a, T>(i: T, value: Progress) -> usize
+where T: Iterator<Item = &'a Progress> {
+    i.filter(|val| *val == &value).count()
 }
 
 #[cfg(test)]
